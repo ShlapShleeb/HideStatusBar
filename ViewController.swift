53a8a21eea9c92b7bb46eadfc390980bfabf7195
ShlapShleeb <ShlapShleeb@gmail.com>
2016-08-25 22:18:38 +0300

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  HideStatusBar
+//
+//  Created by Shlap Shleeb on 25‏/8‏/2016.
+//  Copyright © 2016 ShlapShleeb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+    
+}
+
